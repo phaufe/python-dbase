6efecf9073db91d6b71034da2d45129fbaf3c44b
David Kwast <david@kwast.net>
2016-01-17 19:48:15 -0200

    docstrings added

--- dbase.py
+++ dbase.py
@@ -7,26 +7,37 @@ from datetime import datetime
 from collections import namedtuple
 
 class DbfFieldType(object):
+    '''dBase field type class
+    parameters: dbase binary value, dbase field description
+    '''
+    
     def __init__(self, dbf_type, name):
         self.dbf_type = dbf_type
         self.name = name
     
     def to_dbf(self, field_descriptor, value):
+        '''converts to binary form of a dbase field'''
+        # empty value
         if value is None:
             return ' ' * field_descriptor.field_length
+        # char type
         elif self.dbf_type == 'C':
             return value.ljust( field_descriptor.field_length )
+        # number type
         elif self.dbf_type == 'N':
             lenght = field_descriptor.field_length
-            if value < 0:
+            if value < 0: # negative number
                 lenght += -1
             string = '{: >'+ str( field_descriptor.field_length ) +'.'+ str( field_descriptor.field_decimal_places ) + 'f}'
             return string.format( value )
+        # date type
         elif self.dbf_type == 'D':
             return '{:04d}{:02d}{:02d}'.format( value.year, value.month, value.day )
+        # other types, not implemented
         else:
             raise ValueError( 'DbfFieldType convert error' )
 
+# a namespace to organize dbase field types
 types = namedtuple( 'DbfTypes', [ 'Char', 'Date', 'Logical', 'Memo', 'Numerical' ] )(
     Char = DbfFieldType( 'C', 'Char' ),
     Date = DbfFieldType( 'D', 'Date' ),
@@ -35,7 +46,7 @@ types = namedtuple( 'DbfTypes', [ 'Char', 'Date', 'Logical', 'Memo', 'Numerical'
     Numerical = DbfFieldType( 'N', 'Numerical' ),
 )
 
-
+# all dbase versions
 DBF_FILE_TYPE = {
     0x02: 'FoxBASE',
     0x03: 'FoxBASE+/Dbase III plus, no memo',
@@ -54,10 +65,13 @@ DBF_FILE_TYPE = {
 
 
 class DbfHeaderLastUpdate():
+    '''class to represent dbase header last update record'''
+    
     def __init__(self, datetime):
         self.datetime = datetime
     
     def to_tuple(self):
+        '''converts internal datetime object to a tuple in the correct order'''
         return (
             self.datetime.year - 2000,
             self.datetime.month,
@@ -65,6 +79,8 @@ class DbfHeaderLastUpdate():
         )
 
 class DbfHeader(object):
+    '''class to read and write dbase file header'''
+    
     STRUCT = '<4BI2H20x' # struct for DBASE III only
     HEADER_TERMINATOR = chr( 0x0D )
     
@@ -77,6 +93,7 @@ class DbfHeader(object):
         self.field_descriptors = field_descriptors
     
     def to_binary(self):
+        '''converts header data to binary form'''
         last_update_year, last_update_month, last_update_day = self.last_update.to_tuple()
         header_01 = struct.pack( self.STRUCT,
             self.version,
@@ -92,6 +109,8 @@ class DbfHeader(object):
         return header_01 + header_02 + header_03
 
 class DbfHeaderFieldDescriptor(object):
+    '''class to read and write dbase field descriptors'''
+    
     STRUCT = '<11scI2B14x'
     
     def __init__(self, field_data_address, field_name, field_type, field_length, field_decimal_places):
@@ -102,6 +121,7 @@ class DbfHeaderFieldDescriptor(object):
         self.field_decimal_places = field_decimal_places
     
     def to_binary(self):
+        '''converts field descriptor data to binary form'''
         return struct.pack( self.STRUCT,
             self.field_name.ljust( 11, chr( 0x00 ) ),
             self.field_type.dbf_type,
@@ -161,17 +181,21 @@ class DBF(object):
             #self.header = DbfHeader( fd.read() )
     
     def _write_record(self, record_tuple):
+        '''writes a single record, assumes that the file pointer is in the right place'''
         self._fd.write(' ') # FLAG DELETE ou nao
         for value, field_descriptor in zip( record_tuple, self.header.field_descriptors ):
             self._fd.write( field_descriptor.field_type.to_dbf( field_descriptor, value ) )
     
     def _write_terminator(self):
+        '''writes dbase file terminator symbol, assumes that the file pointer is in the right place'''
         self._fd.write( self.DFB_FILE_TERMINATOR )
     
     def _go_to_end_of_file(self, offset = 0):
+        '''changes file pointer to the end of file'''
         self._fd.seek( offset, os.SEEK_END )
     
     def _find_and_remove_terminator(self):
+        '''goes to end of file and removes the 'dbase file terminator symbol' if it exists'''
         self._go_to_end_of_file( -1 )
         last_char = self._fd.read( 1 )
         if last_char == self.DFB_FILE_TERMINATOR:
@@ -179,6 +203,7 @@ class DBF(object):
             self._fd.truncate( self._fd.tell() )
     
     def _read_n_records(self):
+        '''reads the value of dbase file 'number of records' from the header'''
         fd_pos = self._fd.tell()
         self._fd.seek( 4 )
         value = ord( self._fd.read( 1 ) )
@@ -186,12 +211,14 @@ class DBF(object):
         return value
     
     def _update_n_records(self, value = 1):
+        '''updates the value of dbase file 'number of records' in the header'''
         fd_pos = self._fd.tell()
         self._fd.seek( 4 )
         self._fd.write( chr( value + self._read_n_records() ) )
         self._fd.seek( fd_pos )
     
     def append(self, record_tuple):
+        '''appends a record in the end of the file, dealing with the dbase terminator field'''
         self._find_and_remove_terminator()
         self._write_record( record_tuple )
         self._update_n_records( 1 )
